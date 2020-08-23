693f02ef8b380c1b53e74da4df0af9c40675b117
miocid152 <lomen_ro_152@hotmail.com>
2020-08-23 18:37:58 -0500

    nothing

--- libSecurity/src/main/java/com/er/carrascome/libsecurity/resource/SecurityConstants.java
+++ libSecurity/src/main/java/com/er/carrascome/libsecurity/resource/SecurityConstants.java
@@ -2,5 +2,5 @@ package com.er.carrascome.libsecurity.resource;
 
 public class SecurityConstants {
 
-    private static String hexky = "0501dc5155ea4bcacf0924b2140503eb874f3ef84156c1588f004c53cc359a91eef65507c3a7b4460f0938bbec5a9d061be666d14808673c74a1a5ebab4574c9";
+    public static String hexky = "0501dc5155ea4bcacf0924b2140503eb874f3ef84156c1588f004c53cc359a91eef65507c3a7b4460f0938bbec5a9d061be666d14808673c74a1a5ebab4574c9";
 }
